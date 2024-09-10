aa94e8d66ef2fbbe9fc1c7049479bbe934629549
Tr100000 <147051084+Tr100000@users.noreply.github.com>
2024-09-10 21:08:50 +0900

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -11,4 +10,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: "/html-preview/",
 });
